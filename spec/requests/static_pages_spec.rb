aad027c67ed7805626c5a7bf8e8fbddcdb233e19
Trunks Li <trunksli@hotmail.com>
2013-06-16 19:01:48 -0400

    contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "TEST H1 HOME-should have the h1 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "TEST TITLE HOME should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Home"
+      expect(page).to have_title "#{base_title} | Home"
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "TEST TITLE HELP should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Help"
+      expect(page).to have_title "#{base_title} | Help"
     end
   end
 
@@ -37,8 +39,22 @@ describe "Static pages" do
 
     it "TEST TITLE ABOUT should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title "About Us"
+      expect(page).to have_title "#{base_title} | About Us"
     end
   end
+
+  describe "Contact" do
+
+    it "TEST H1 contact should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "TEST TITLE CONTACT should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title "#{base_title} | Contact"
+    end
+  end
+
 
 end
